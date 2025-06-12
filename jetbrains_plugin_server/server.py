ea8d49d1517817f8009eccc6becc2def934da4eb
azro352 <35503478+azro352@users.noreply.github.com>
2025-06-12 20:27:38 +0200

    refactor(packages): split into a router

--- jetbrains_plugin_server/server.py
+++ jetbrains_plugin_server/server.py
@@ -12,10 +12,10 @@ else:
 
 from fastapi.responses import HTMLResponse, Response
 
+from jetbrains_plugin_server.model import packages
 from jetbrains_plugin_server.model.errors import add_error_handler
 from jetbrains_plugin_server.plugin_catalog import get_plugin_catalog
 from jetbrains_plugin_server.plugin_model import get_plugins
-from jetbrains_plugin_server.schemas import PluginSchema
 from jetbrains_plugin_server.to_xml import to_xml
 
 LOG = logging.getLogger(__name__)
@@ -25,6 +25,8 @@ def create_app():
     app = FastAPI()
 
     add_error_handler(app)
+
+    app.include_router(packages.router)
 
     @app.get("/")
     def get_plugins_route(build: Annotated[
@@ -45,26 +47,5 @@ def create_app():
     @app.get("/cache")
     def get_cache_route():
         return get_plugin_catalog()
-
-    @app.get("/packages")
-    def get_packages_route():
-        catalog = get_plugin_catalog()
-        md = "# Packages\n"
-        md += "\n".join(f"- [{p.name}](/packages/{p.versions[0].plugin_id})" for p in catalog.plugins)
-        md += "\n\n [Previous page](/)"
-        return HTMLResponse(content=markdown.markdown(md))
-
-    @app.get("/packages/{plugin_id}")
-    def get_package_route(plugin_id: str):
-        catalog = get_plugin_catalog()
-        plugin: PluginSchema = next(p for p in catalog.plugins if p.versions[0].plugin_id == plugin_id)
-        md = f"# Package {plugin.name}\n"
-        md += "\n".join(
-            f"- [{v.version}](/packages/{plugin_id}/{v.version}) "
-            f"compatible with IDE version in `[{v.specs.since_build} ; {v.specs.until_build}]`"
-            for v in plugin.versions
-        )
-        md += "\n\n [Previous page](/packages)"
-        return HTMLResponse(content=markdown.markdown(md))
 
     return app
